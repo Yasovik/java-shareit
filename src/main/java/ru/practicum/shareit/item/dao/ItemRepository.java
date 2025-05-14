d058de32bb7215fb6616ca919b3e1d2ae2a6cb52
Alexandr Zakurdaev <azakyrdaev@gmail.com>
2025-05-14 21:03:54 +0300

    Финальное задание 14го спринта.

---
+++ src/main/java/ru/practicum/shareit/item/dao/ItemRepository.java
@@ -0,0 +1,85 @@
+package ru.practicum.shareit.item.dao;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemDtoMapper;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.*;
+
+@Slf4j
+@Repository
+public class ItemRepository {
+    private final Map<Integer, Map<Integer, Item>> items = new HashMap<>();
+    private int id = 1;
+
+    public Item create(ItemDto itemDto, int owner) {
+        Item item = ItemDtoMapper.toItemDto(itemDto, id, owner);
+        Map<Integer, Item> itemsMap;
+        if (items.containsKey(owner)) {
+            itemsMap = items.get(owner);
+        } else {
+            itemsMap = new HashMap<>();
+        }
+        itemsMap.put(id, item);
+        items.put(owner, itemsMap);
+        id++;
+        log.info("Вещь успешно создана: {}", item);
+        return item;
+    }
+
+    public Item update(ItemDto itemDto, int owner, int id) {
+        Map<Integer, Item> itemsMap = items.get(owner);
+        Item oldItem = itemsMap.get(id);
+
+        if (itemDto.getName() != null && !itemDto.getName().isEmpty() && !itemDto.getName().isBlank()) {
+            oldItem.setName(itemDto.getName());
+        }
+        if (itemDto.getDescription() != null && !itemDto.getDescription().isEmpty() && !itemDto.getDescription().isBlank()) {
+            oldItem.setDescription(itemDto.getDescription());
+        }
+        if (itemDto.getAvailable() != null) {
+            oldItem.setAvailable(itemDto.getAvailable());
+        }
+        if (itemDto.getRequestId() != null) {
+            oldItem.setRequest(itemDto.getRequestId());
+        }
+        itemsMap.put(id, oldItem);
+        items.put(owner, itemsMap);
+        log.info("Вещь успешно обновлена: {}", itemDto);
+        return oldItem;
+    }
+
+    public Item getItem(int id) {
+        return items.values().stream()
+                .map(map -> map.get(id))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Вещь с ID " + id + " не найдена"));
+
+    }
+
+    public List<Item> getItemsForOwner(int owner) {
+        try {
+            return items.get(owner).values().stream().toList();
+        } catch (NullPointerException e) {
+            throw new NotFoundException("Вещи для пользователя с ID " + id + " не найдены");
+        }
+    }
+
+    public List<Item> searchItems(String text) {
+        try {
+            return items.values().stream()
+                    .flatMap(userItems -> userItems.values().stream())
+                    .filter(Objects::nonNull)
+                    .filter(item -> (item.getName().toLowerCase().contains(text) ||
+                            item.getDescription().toLowerCase().contains(text)) &&
+                            item.getAvailable())
+                    .toList();
+        } catch (NullPointerException e) {
+            return new ArrayList<>();
+        }
+    }
+}
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,28 @@
 package ru.practicum.shareit.item.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.other.Marker;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ItemDto {
+    @NotBlank(message = "Имя не может быть пустым", groups = Marker.OnCreate.class)
+    private String name;
+    @NotBlank(message = "Описание не может быть пустым", groups = Marker.OnCreate.class)
+    @Size(max = 300, message = "Длина описания не должна превышать 300 символов")
+    private String description;
+    @NotNull(message = "Поле доступности не можеть быть пыстым", groups = Marker.OnCreate.class)
+    private Boolean available;
+    private Integer requestId;
 }
